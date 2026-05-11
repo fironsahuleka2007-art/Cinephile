deae31bb6d9a804e670ff485689fb5c8d8f713ee
GharinFawwaz <gharinfawwaz0103@gmail.com>
2026-05-11 15:14:37 +0700

    new UI

--- genreAnalyze.py
+++ genreAnalyze.py
@@ -85,6 +85,7 @@ class GenreAnalyzePage(ctk.CTkFrame):
         ctk.CTkButton(pill, text="Movie Table", width=92, height=28, fg_color="transparent", text_color=TEXT_GRAY, corner_radius=16, font=("Trebuchet MS", 11, "bold"), command=lambda: self.app.show_page("movietable")).pack(side="left", padx=1)
         ctk.CTkButton(pill, text="Watchlist", width=80, height=28, fg_color="transparent", text_color=TEXT_GRAY, corner_radius=16, font=("Trebuchet MS", 11, "bold"), command=lambda: self.app.show_page("watchlist")).pack(side="left", padx=3)
 
+#ini ysng baru
     def create_hero_section(self):
         ctk.CTkLabel(self.body, text="Genre Analyze", font=("Helvetica", 70, "bold"), text_color=TEXT_WHITE).pack(pady=(60, 20))
 
@@ -139,7 +140,7 @@ class GenreAnalyzePage(ctk.CTkFrame):
                          "frequently, providing insights through an easy-to-read graphical interface.")
         ctk.CTkLabel(desc_frame, text=overview_text, font=("Trebuchet MS", 13), text_color=TEXT_WHITE, justify="left").pack(side="left", padx=40)
 
-#ini ysng baru
+
     def _animate_carousel(self):
         """Geser carousel ke kiri tiap 30ms"""
         try:
